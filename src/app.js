32c4493b4b082d159c5eeee4e42003b44d9ea44f
StigAkl <stg@hotmail.no>
2019-05-20 20:27:56 +0200

    Add index route for deploy badge

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ app.use(express.json());
 app.use(playerRouter);
 
 
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome!"); 
+})
 app.listen(port, (req, res) => {
     console.log("Listening on port ", port); 
 }); 
