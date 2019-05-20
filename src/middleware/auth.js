69bf817a46ce6d9f7bd6d9ac2bad20e110f012d4
StigAkl <stg@hotmail.no>
2019-05-20 20:21:42 +0200

    Bugfix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -16,7 +16,7 @@ const auth = async (req, res, next) => {
         
     } catch(error) {
         res.status(401).send({
-            error: "Unathorized" + error
+            error: "Unathorized"
         })
     }
 }
