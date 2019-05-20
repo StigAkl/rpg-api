cd7fd2536ff09c657c0a19049dd560daec97501a
StigAkl <stg@hotmail.no>
2019-05-20 04:56:17 +0200

    Change port for heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('./db/mongoose'); 
 const express = require("express"); 
-const port = process.env.port || 3001; 
+const port = process.env.PORT || 3001; 
 const app = express(); 
 const Player = require('./entities/player'); 
 
