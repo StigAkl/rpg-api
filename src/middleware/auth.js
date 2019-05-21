b37f7e6be170c2b711e44af034c2c51adab9e404
StigAkl <stg@hotmail.no>
2019-05-21 19:24:58 +0200

    Fix:Add env variable for token secret

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken"); 
 const Player = require("../entities/player");
-
+const tokenSecret = process.env.TOKEN_SECRET || "playerSecret"; 
 const auth = async (req, res, next) => {
 
     try {
         const token = req.header("Authorization").replace("Bearer ", ""); 
-        const decodedToken = jwt.verify(token, "playerSecret")
+        const decodedToken = jwt.verify(token, tokenSecret)
 
         const player = await Player.findOne({_id: decodedToken._id, "token": token}); 
 
