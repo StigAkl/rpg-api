d907e83e5ca264f0a9edd3ed7a143b8528bb5667
StigAkl <stg@hotmail.no>
2019-05-21 19:17:13 +0200

    Enable cors stuff

--- src/app.js
+++ src/app.js
@@ -5,12 +5,24 @@ const port = process.env.PORT || 3001;
 const app = express(); 
 const playerRouter = require("./routers/player-router"); 
 
+var allowedOrigins = ['http://localhost:3000',
+                      'https://rpg-adventures-v1.herokuapp.com'];
 app.use(cors({
-    origin: "http://localhost:3000"
-})); 
+origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    
+    if(allowedOrigins.indexOf(origin) === -1){
+    var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+    return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+}
+}));
 
 app.use(express.json()); 
 app.use(playerRouter);
+
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome!"); 
